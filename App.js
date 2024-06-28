6fd060792a9ac803f85a01b04f5fdb846b9e5f26
nessiM <nessim.ben-haj-ali.externe@foncia.fr>
2024-06-28 21:53:38 +0200

    feat(boilerplate): expo boilerplate

---
+++ App.js
@@ -0,0 +1,24 @@
+// `@expo/metro-runtime` MUST be the first import to ensure Fast Refresh works
+// on web.
+import '@expo/metro-runtime';
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.ts to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++
@@ -1,2 +0,0 @@
-# triathlon-pace
-Find your speed in swim, run, bike on a single app
